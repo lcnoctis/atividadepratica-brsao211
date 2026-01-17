c254902fadb9aa03a142b35f0a4ef46cababe403
lcnoctis <ellenjack2014@email.com>
2026-01-17 17:41:39 -0300

    atvd 2 - qt 4

---
+++ atividadepratica02/questão4.py
@@ -0,0 +1,9 @@
+
+distancia = 300  # km
+combustivel = 25  # litros
+
+consumo_medio = distancia / combustivel
+
+print("Distância percorrida:", distancia, "km")
+print("Combustível gasto:", combustivel, "litros")
+print("Consumo médio:", round(consumo_medio, 2), "km/l")
